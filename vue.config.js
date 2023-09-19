f117b7a847456562131fc9ea71b32d4ef8160563
Amith Bino <amith1062@gmail.com>
2023-09-19 21:36:09 +0530

    deploy

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,7 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true,
+  publicPath:'anotherwebsite',
   chainWebpack: (config) => {
     config.module
       .rule('pdf')
